1f2e6666708db63611a3cf3cd298e121a9b729a7
yogurtcheesedope <151974519+yogurtcheesedope@users.noreply.github.com>
2023-12-06 03:47:40 -0500

    Update movement_faces.h

--- movement/movement_faces.h
+++ movement/movement_faces.h
@@ -46,7 +46,7 @@
 #include "hello_there_face.h"
 #include "sunrise_sunset_face.h"
 #include "countdown_face.h"
-#include "countdown_face_reset.h"
+#include "countdown_reset_face.h"
 #include "sailing_face.h"
 #include "counter_face.h"
 #include "blinky_face.h"
